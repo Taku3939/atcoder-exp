b45492892bc1327f4aa1323a57b591efaf2247e7
Taku <sakisaki200012@gmail.com>
2022-08-14 19:05:03 +0900

    initial commit

---
+++ begin.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+cd `dirname $0`
+dir=`pwd`
+
+echo $dir
+
+contest=$1
+
+echo "Create contest contest directory..."
+mkdir $contest
+cd $contest
+
+
+echo "Create files."
+
+# cpp file template
+template=$(cat <<EOS
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int result { 0 };
+
+    ////////////////////
+    // Write your code.
+    ////////////////////
+    
+    cout << result << endl;
+
+    return 0;
+}
+EOS
+)
+
+filelist=("a" "b" "c" "d" "e" "f")
+
+# shellcheck disable=SC2068
+for file in ${filelist[@]}
+do
+    touch ${file}.cpp
+    echo "$template" > ${file}.cpp
+    echo "created file: ${file}.cpp"
+done
+
+echo "Add executable into CMakeLists.txt"
+
+cd ..
+
+# Write settings into CMakeLists.txt
+echo "" >> ./CMakeLists.txt
+echo "# Contest $contest" >> ./CMakeLists.txt
+
+# shellcheck disable=SC2068
+for file in ${filelist[@]}
+do
+    echo "add_executable(${contest}_${file} $contest/${file}.cpp)" >> ./CMakeLists.txt
+done
